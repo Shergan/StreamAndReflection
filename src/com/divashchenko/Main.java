47841b1da485b46d9a34a1af7f04efee106daa38
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-11 20:39:31 +0200

    Initial commit

---
+++ src/com/divashchenko/Main.java
@@ -0,0 +1,52 @@
+package com.divashchenko;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            list.add(random.nextInt(10));
+        }
+
+        double ans = 0;
+        for (int i = 0; i < list.size(); i++) {
+            ans += (list.get(i) * list.get(i));
+        }
+        ans /= list.size();
+        System.out.println("Standard method: " + ans);
+
+
+        System.out.println(list);
+
+
+        Double answer = list
+                .stream()
+                .map(x -> Math.pow(x, 2) / list.size())
+                .reduce(0.0, (x1, x2) -> x1 + x2);
+
+        System.out.println("Stream method: " + answer);
+        System.out.println();
+
+
+        //Reflection
+        String className = "test3";
+        TestReflection testReflection = new TestReflection();
+
+        try {
+            Method method = testReflection.getClass().getDeclaredMethod(className);
+            method.invoke(testReflection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/com/divashchenko/TestReflection.java
@@ -0,0 +1,19 @@
+package com.divashchenko;
+
+public class TestReflection {
+    public void test1() {
+        System.out.println("Test 1 from TestReflection");
+    }
+    public void test2() {
+        System.out.println("Test 2 from TestReflection");
+    }
+    public void test3() {
+        System.out.println("Test 3 from TestReflection");
+    }
+    public void test4() {
+        System.out.println("Test 4 from TestReflection");
+    }
+    public void test5() {
+        System.out.println("Test 5 from TestReflection");
+    }
+}
